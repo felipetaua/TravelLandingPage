c4eea6efc7934387e9057b06033239f19048208f
felipetaua <taua.felipee@gmail.com>
2025-09-25 22:24:59 -0300

    adicionando a funcionalidade de fechar o menu ao clicar fora do menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 const navMenu = document.getElementById('nav-menu')
 const menuHamburguer = document.getElementById('menu-hamburguer')
 
-menuHamburguer.addEventListener('click', () => {
+
+function toggleMenu() {
     navMenu.classList.toggle('active')
+}
+
+menuHamburguer.addEventListener('click', toggleMenu)
+
+document.addEventListener('click', (event) => {
+    const isMenuOpen = navMenu.classList.contains('active')
+    
+    if (isMenuOpen && !navMenu.contains(event.target) && !menuHamburguer.contains(event.target)) {
+        navMenu.classList.remove('active')
+    }
 })
 
